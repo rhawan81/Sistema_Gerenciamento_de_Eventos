c013e77273e5bab948e56958c838832dfac064a8
Rhawan81 <rhawanjs@gmail.com>
2025-09-26 00:51:59 -0300

    'Implementei o metodo na classe Eventos para exibir os detalhes'

--- evento.py
+++ evento.py
@@ -1,4 +1,4 @@
-class Evento():
+class Evento:
     def __init__(self, nome, data, local, capacidade_maxima, categoria, preco):
         self.__nome = nome
         self.__data = data
@@ -7,3 +7,18 @@ class Evento():
         self.__categoria = categoria
         self.__preco = preco
         self.__inscritos = []
+    def get_detalhes(self):
+        print(f'Nome: {self.__nome}\n Data: {self.__data}\n Local: {self.__local}\n Capacidade: {self.__capacidade_maxima}\n Categoria: {self.__categoria}\n Preco: {self.__preco}\n Inscritos: {self.__inscritos}')
+class Workshop(Evento):
+    def __init__(self, nome, data, local, capacidade_maxima, categoria, preco,material_necessario):
+        super().__init__(nome, data, local, capacidade_maxima, categoria, preco)
+        self.__material_necessario = material_necessario
+
+
+class Palestra(Evento):
+    pass
+    
+    
+    
+class Participante:
+    pass
